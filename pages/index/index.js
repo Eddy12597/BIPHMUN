64b53b92bdb4d291df3af3cd9618e5ad06602b5a
Eddy12597 <eddy12597@163.com>
2025-07-21 20:46:59 +0800

    added info section; created parallax effect; added github link

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,3 +3,17 @@ dropdownBtn.addEventListener('click', function() {
     const dropdownContent = document.getElementById('link-icons-section');
     dropdownContent.classList.toggle('active');
 });
+
+const parallaxCoef = 0.4;
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const parallax = document.getElementById('group-photo');
+
+    window.addEventListener('scroll', function () {
+        const parallax = document.getElementById('group-photo');
+        const scrollY = window.scrollY;
+        parallax.style.transform = `translateY(${scrollY * parallaxCoef}px)`;
+        // parallax.style.backgroundPositionY = `${scrollY * parallaxCoef}px`;
+    });
+
+});
