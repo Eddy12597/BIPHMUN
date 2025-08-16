52af62d520bc4c3e50656cbfaf3a002729b1efee
Eddy12597 <eddy12597@163.com>
2025-08-16 18:04:01 +0800

    feat: enhance application buttons with hover effects and JS handlers
    fix: change application links to interactive buttons with hover effects
    feat: add JavaScript handlers for application button clicks
    style: update button styling and add hover transition effects

---
+++ pages/application/application.js
@@ -0,0 +1,24 @@
+const individualApplicationFormLink = 'https://forms.office.com/r/xGJjib7nGN';
+
+// though its a pdf
+const schoolApplicationFormLink = 'assets/documents/school-app.pdf';
+
+const chairApplicationFormLink = 'https://forms.office.com/r/CjfTJMapVf';
+
+const volunteerApplicationFormLink = 'https://example.com';
+
+const handleIndv = () => {
+  window.open(individualApplicationFormLink, '_blank');
+}
+
+const handleSchool = () => {
+  window.open(schoolApplicationFormLink, '_blank');
+}
+
+const handleChair = () => {
+  window.open(chairApplicationFormLink, '_blank');
+}
+
+const handleVolunteer = () => {
+  window.open(volunteerApplicationFormLink, '_blank')
+}
